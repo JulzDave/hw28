8ff9bb9277cb783b25a106fcaf6d9e45a2c8970f
Julian <juliandave1989@gmail.com>
2019-01-20 22:58:02 +0200

    Commit test

--- routes/index.js
+++ routes/index.js
@@ -10,87 +10,62 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 
+router.post('/reg', function (req, res, next) {
+  if (req.body.name != "" && req.body.age != "" && req.body.city != "" && req.body.salary) {
+    let insertQuery = `
+    INSERT INTO workers (name, age, city, salary) 
+    VALUES ('${req.body.name}',  ${req.body.age}, '${req.body.city}', ${req.body.salary})
+  `
 
-router.get('/deleter', function(req, res, next) {
-  console.log("oved!")
-  let  x= parseInt(req.query.worker_id);
-  connection.query(`DELETE FROM workers WHERE id = '${x}'`, (err, data) => {
-    if (err) throw err;
-    console.log("Number of workers deleted: " + data.affectedRows);
-     res.redirect("/allworkers")
-    // res.send(""+(x+5));
-});
-});
-
-function deleter (err) {
-  if (err) throw err;
-  //Delete all customers with the address "Mountain 21":
-  // var sql = ;
-  // connection.query("DELETE FROM workers WHERE id = 'Mountain 21'", function (err, result) {
-  //   if (err) throw err;
-  //   console.log("Number of records deleted: " + result.affectedRows);
-  //    res.redirect("/allworkers")
-  // });
-};
-
-
-
-
-router.post('/reg', function(req, res, next) {
-  
-  let insertQuery=`
-  INSERT INTO workers (name, age, city, salary) 
-  VALUES ('${req.body.name }',  ${req.body.age}, '${req.body.city}', ${req.body.salary})
-  ` 
-
-  connection.query(insertQuery, (err, data)=>
-  {
-      if(err) throw err;
+    connection.query(insertQuery, (err, data) => {
+      if (err) throw err;
       res.redirect("/allworkers");
-  }); 
-   
+    });
+  }
+  else {
+    res.redirect("/");
+  }
+
 });
 
 
-router.get('/allworkers', function(req, res, next) {
+router.get('/allworkers', function (req, res, next) {
 
-  connection.query('SELECT * FROM workers', (err, data)=>
-  {
-      if(err) throw err;
+  connection.query('SELECT * FROM workers', (err, data) => {
+    if (err) throw err;
 
-      let htmlRes=`
-      <style>
-      body{
-        background:black;
-        color:greenyellow;
-      }
-      table{
-        border:1px ridge green;
-      }
-      td{
-        border: 1px ridge green;
-      }
+    let htmlRes = `
+    <style>
+
+    body{
+      background:black;
+      color:greenyellow;
+    }
+
+    table{
+      border:3px ridge limegreen;
+    }
+
+    td{
+      border: 1px ridge green;
+    }
+    
+    th{
+      border: 3px ridge limegreen;
+    }
+    
+    .delBtn{
+      cursor:pointer;
+    }
+
+    input[type='button']{
+      cursor:pointer;
+    } 
       
-      th{
-        border: 1px ridge green;
-      }
-
-      .delBtn{
-        cursor:pointer;
-      }
-      input[type='button']{
-        cursor:pointer;
-      }
-
       </style>
-
-
+      
       <table style='zoom: 175%;font-family: monospace;'>
       <tr>
       <th>ID</th>
@@ -98,29 +73,41 @@ router.get('/allworkers', function(req, res, next) {
       <th>Age</th>
       <th>City</th>
       <th>Salary</th>
+      <th>Remove Worker</th>
       </tr>
-      `; 
+      `;
 
-      data.forEach(worker => {
-          htmlRes+= `<tr> 
-          <td>${worker.ID}</td> 
-          <td>${worker.name}</td> 
-          <td>${worker.age}</td>
-          <td>${worker.city}</td> 
-          <td>${worker.salary}</td>
-          <td><button class="delBtn" onclick="location.href = '/deleter?worker_id=${worker.ID} ' " style='background:green;color:black;font-family:monospace;width:113px;'>Remove ${worker.name}</button></td>
+    data.forEach(worker => {
+      htmlRes += `<tr> 
+        <td>${worker.ID}</td> 
+        <td>${worker.name}</td> 
+        <td>${worker.age}</td>
+        <td>${worker.city}</td> 
+        <td>${worker.salary}</td>
+          <td><button class="delBtn" onclick="location.href = '/deleter?worker_id=${worker.ID} ' " 
+          style='background:green;color:black;font-family:monospace;width:113px;'>Remove ${worker.name}</button></td>
           </tr> `
-      }); 
-      htmlRes+= `</table><br/><br/>
-      <input type="button" value="Go Back" onclick="location.href='/'" style='background:green;color:yellow;font-size:18px;font-family:monospace;width:113px;' /> 
-      `; 
+    });
+    htmlRes += `</table><br/><br/>
+        <input type="button" value="&#x261A Go Back" onclick="location.href='/'" style='background:green;color:yellow;font-size:18px;font-family:monospace;width:113px;' /> 
+        `;
 
-     // res.json(data);
-      res.send(htmlRes);
+    // res.json(data);
+    res.send(htmlRes);
   });
 
 });
 
+router.get('/deleter', function (req, res, next) {
+ 
+  let x = parseInt(req.query.worker_id);
+  connection.query(`DELETE FROM workers WHERE id = '${x}'`, (err, data) => {
+    if (err) throw err;
+    console.log("Number of workers deleted: " + data.affectedRows);
+    res.redirect("/allworkers")
+    // res.send(""+(x+5));
+  });
+});
 
 
 //---------------------------------------------------------------
